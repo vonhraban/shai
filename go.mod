181d152f305b151b4d38ffcabd9904c848af2c89
Vlad <23172727+vonhraban@users.noreply.github.com>
2024-01-02 22:23:09 +0700

    Output command output in non-blocking way

--- go.mod
+++ go.mod
@@ -4,4 +4,19 @@ go 1.21.5
 
 // replace github.com/vonhraban/shai => ./
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/manifoldco/promptui v0.9.0
+	github.com/pieterclaerhout/go-log v1.15.0
+)
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rotisserie/eris v0.5.4 // indirect
+	github.com/sanity-io/litter v1.5.5 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/vonhraban/shai/internal/openai_client"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/manifoldco/promptui"
 )
 
 var debug bool
@@ -30,17 +33,69 @@ func main() {
 		return
 	}
 
-	// Set your conversation prompt here
-	//prompt := "What is square root of 49"
-	prompt := input
-
 	client := openai_client.NewChatGPTClient(apiKey)
 	client.SetDebug(debug)
-	
-	res, err := client.PromptCompletions(prompt)
+
+	res, err := client.PromptCompletions(input)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(res)
+	prompt := promptui.Prompt{
+		Label:     "Run command?",
+		IsConfirm: true,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if result == "y" {
+		shell := os.Getenv("SHELL")
+
+		cmd := exec.Command(shell, "-c", res)
+
+		//Get a pipe to read from standard out
+		r, _ := cmd.StdoutPipe()
+
+		// Use the same pipe for standard error
+		cmd.Stderr = cmd.Stdout
+
+		// Make a new channel which will be used to ensure we get all output
+		done := make(chan struct{})
+
+		// Create a scanner which scans r in a line-by-line fashion
+		scanner := bufio.NewScanner(r)
+
+		// Use the scanner to scan the output line by line and log it
+		// It's running in a goroutine so that it doesn't block
+		go func() {
+
+			// Read line by line and process it
+			for scanner.Scan() {
+				line := scanner.Text()
+				fmt.Println(line)
+			}
+
+			// We're all done, unblock the channel
+			done <- struct{}{}
+
+		}()
+
+		// Start the command and check for errors
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("can not run command: %w", err)
+		}
+
+		// Wait for all output to be processed
+		<-done
+
+		// Wait for the command to finish
+		err = cmd.Wait()
+	}
 }
