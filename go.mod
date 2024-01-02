1fb7c1b87c996ddcf66d5219f5396d5061dc8132
Vlad <23172727+vonhraban@users.noreply.github.com>
2024-01-02 23:55:02 +0700

    Add all the cli interactivity

--- go.mod
+++ go.mod
@@ -11,12 +11,24 @@ require (
 )
 
 require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pterm/pterm v0.12.73 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rotisserie/eris v0.5.4 // indirect
 	github.com/sanity-io/litter v1.5.5 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 
 	"github.com/joho/godotenv"
-	"github.com/manifoldco/promptui"
+	"github.com/pterm/pterm"
 )
 
 var debug bool
@@ -18,15 +18,18 @@ var apiKey string
 func main() {
 	godotenv.Load()
 	apiKey = os.Getenv("SHAI_OPENAI_API_KEY")
-	_, debug = os.LookupEnv("SHAI_DEBUG")
-
-	args := os.Args
-	if len(args) == 1 {
-		fmt.Printf("Usage: %s <plain text query | setup> \n", args[0])
-		os.Exit(1)
+	debugEnvVar := os.Getenv("SHAI_DEBUG")
+	if debugEnvVar != "" && debugEnvVar != "0" {
+		debug = true
 	}
 
+	args := os.Args
+
 	input := strings.Join(args[1:], " ")
+
+	for input == "" {
+		input = askPrompt("")
+	}
 
 	if input == "setup" {
 		fmt.Println("Setup wizard")
@@ -36,66 +39,116 @@ func main() {
 	client := openai_client.NewChatGPTClient(apiKey)
 	client.SetDebug(debug)
 
+	command := promptForCommand(client, input)
+
+	if command == "" {
+		pterm.Warning.Println("Cancelled")
+		os.Exit(0)
+	}
+
+	pterm.Info.Printfln("Executing")
+
+	executeCommand(command)
+}
+
+func promptForCommand(client *openai_client.ChatGPTClient, input string) string {
+	// Create a multi printer. This allows multiple spinners to print simultaneously.
+	multi := pterm.DefaultMultiPrinter
+	writer := multi.NewWriter()
+	// Create and start spinner 1 with a new writer from the multi printer.
+	// The spinner will display the message "Spinner 1".
+	spinner1, _ := pterm.DefaultSpinner.WithWriter(writer).Start("Loading")
+	multi.Start()
+
 	res, err := client.PromptCompletions(input)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(res)
-	prompt := promptui.Prompt{
-		Label:     "Run command?",
-		IsConfirm: true,
+	spinner1.Success("Completed")
+
+	pterm.DefaultHeader.WithWriter(writer).WithMargin(15).WithBackgroundStyle(pterm.NewStyle(pterm.BgCyan)).WithTextStyle(pterm.NewStyle(pterm.FgBlack)).Println(res)
+
+	// Stop the multi printer. This will stop printing all the spinners.
+	multi.Stop()
+
+	// Show an interactive confirmation dialog and get the result.
+	//result, _ := pterm.DefaultInteractiveConfirm.Show()
+
+	// Print a blank line for better readability.
+	pterm.Println()
+
+	options := []string{"Run", "Change Query", "Cancel"}
+
+	selectedOption, _ := pterm.DefaultInteractiveSelect.
+		WithDefaultText("Run the command?").
+		WithDefaultOption("Yes").
+		WithFilter(false).
+		WithOptions(options).
+		Show()
+
+	switch selectedOption {
+	case "Run":
+		return res
+	case "Change Query":
+		input = askPrompt(input)
+		return promptForCommand(client, input)
 	}
 
-	result, err := prompt.Run()
+	// Everything else - terminate
+	return ""
+}
 
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
+func askPrompt(defaultValue string) string {
+	textInput := pterm.DefaultInteractiveTextInput.WithMultiLine(false)
 
-	if result == "y" {
-		shell := os.Getenv("SHELL")
+	// Show the text input and get the result
+	input, _ := textInput.WithDefaultValue(defaultValue).Show()
 
-		cmd := exec.Command(shell, "-c", res)
+	return input
+}
 
-		//Get a pipe to read from standard out
-		r, _ := cmd.StdoutPipe()
+func executeCommand(command string) {
+	shell := os.Getenv("SHELL")
 
-		// Use the same pipe for standard error
-		cmd.Stderr = cmd.Stdout
+	cmd := exec.Command(shell, "-c", command)
 
-		// Make a new channel which will be used to ensure we get all output
-		done := make(chan struct{})
+	//Get a pipe to read from standard out
+	r, _ := cmd.StdoutPipe()
 
-		// Create a scanner which scans r in a line-by-line fashion
-		scanner := bufio.NewScanner(r)
+	// Use the same pipe for standard error
+	cmd.Stderr = cmd.Stdout
 
-		// Use the scanner to scan the output line by line and log it
-		// It's running in a goroutine so that it doesn't block
-		go func() {
+	// Make a new channel which will be used to ensure we get all output
+	done := make(chan struct{})
 
-			// Read line by line and process it
-			for scanner.Scan() {
-				line := scanner.Text()
-				fmt.Println(line)
-			}
+	// Create a scanner which scans r in a line-by-line fashion
+	scanner := bufio.NewScanner(r)
 
-			// We're all done, unblock the channel
-			done <- struct{}{}
+	// Use the scanner to scan the output line by line and log it
+	// It's running in a goroutine so that it doesn't block
+	go func() {
 
-		}()
-
-		// Start the command and check for errors
-		err := cmd.Start()
-		if err != nil {
-			fmt.Println("can not run command: %w", err)
+		// Read line by line and process it
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Println(line)
 		}
 
-		// Wait for all output to be processed
-		<-done
+		// We're all done, unblock the channel
+		done <- struct{}{}
 
-		// Wait for the command to finish
-		err = cmd.Wait()
+	}()
+
+	// Start the command and check for errors
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("can not run command: %w", err)
 	}
+
+	// Wait for all output to be processed
+	<-done
+
+	// Wait for the command to finish
+	err = cmd.Wait()
 }
