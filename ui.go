fede4b711a2c2aec2d12272f63c564442757b268
Vlad <23172727+vonhraban@users.noreply.github.com>
2024-01-10 18:06:15 +0700

    Cleanup some code

--- ui.go
+++ ui.go
@@ -19,25 +19,21 @@ func promptForInputInteractive(client openai_client.Client, input string) string
 	// Create a multi printer. This allows multiple spinners to print simultaneously.
 	multi := pterm.DefaultMultiPrinter
 	writer := multi.NewWriter()
-	// Create and start spinner 1 with a new writer from the multi printer.
-	// The spinner will display the message "Spinner 1".
-	spinner1, _ := pterm.DefaultSpinner.WithWriter(writer).Start("Loading")
+
+	spinner, _ := pterm.DefaultSpinner.WithWriter(writer).Start("Loading")
 	multi.Start()
 
 	res, err := client.PromptCompletions(input)
 	if err != nil {
-		panic(err)
+		panic(err) // TODO! Return err and handle gracefully
 	}
 
-	spinner1.Success("Completed")
+	spinner.Success("Completed")
 
 	pterm.DefaultHeader.WithWriter(writer).WithMargin(15).WithBackgroundStyle(pterm.NewStyle(pterm.BgCyan)).WithTextStyle(pterm.NewStyle(pterm.FgBlack)).Println(res)
 
 	// Stop the multi printer. This will stop printing all the spinners.
 	multi.Stop()
-
-	// Show an interactive confirmation dialog and get the result.
-	//result, _ := pterm.DefaultInteractiveConfirm.Show()
 
 	// Print a blank line for better readability.
 	printNewLine()
